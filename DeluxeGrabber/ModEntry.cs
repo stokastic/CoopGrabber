b892bbf803c900fe6557dfb058c4fcfaf9a64a80
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2018-11-22 20:35:36 -0500

    fix API never made available

--- DeluxeGrabber/ModEntry.cs
+++ DeluxeGrabber/ModEntry.cs
@@ -17,6 +17,8 @@ namespace DeluxeGrabber
         private readonly int FARMING = 0;
         private readonly int FORAGING = 2;
 
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper) {
 
             Config = Helper.ReadConfig<ModConfig>();
@@ -26,6 +28,12 @@ namespace DeluxeGrabber
 
             TimeEvents.AfterDayStarted += TimeEvents_AfterDayStarted;
             LocationEvents.ObjectsChanged += LocationEvents_ObjectsChanged;
+        }
+
+        /// <summary>Get an API that other mods can access. This is always called after <see cref="M:StardewModdingAPI.Mod.Entry(StardewModdingAPI.IModHelper)" />.</summary>
+        public override object GetApi()
+        {
+            return new PrismaticAPI(this.Config);
         }
 
         private void SetForagerLocation(string arg1, string[] arg2) {
